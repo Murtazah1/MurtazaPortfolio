063927593a6637d49fdf3732375187ecae9d1b6b
Murtazah1 <mhx017@gmail.com>
2024-11-05 23:57:48 -0600

    static page commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center text-center pt-8 px-4">
-      <h1 className="text-5xl font-bold mb-4">Welcome to Murtaza's Blog</h1>
+      <h1 className="text-5xl font-bold mb-4">Welcome to Murtaza&apos;s Blog</h1>
       <p className="text-lg mb-2">
         Hi, my name is Muhammad (Murtaza) Husain, and I am a Computer Science Major from the University of Houston.
       </p>
--- src/components/homeButton.tsx
+++ src/components/homeButton.tsx
@@ -5,7 +5,7 @@ const HomeButton: React.FC = () => {
     return (
         <div className="absolute top-4 left-4">
             <Link href="/">
-                <Button variant="ghost" className="font-bold text-lg">Murtaza's Blog</Button>
+                <Button variant="ghost" className="font-bold text-lg">Murtaza&apos;s Blog</Button>
             </Link>
         </div>
     );
